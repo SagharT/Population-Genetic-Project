7934c16ccccce1ca867f86171e23082a6c04c131
Saghar <saghar.k.j@gmail.com>
2023-03-20 19:35:47 +0100

    Update data analysis code to fix comments

--- DataAnalysis.py
+++ DataAnalysis.py
@@ -11,10 +11,8 @@ bim = pd.read_csv('DataS1.bim', sep='\t', header=None)
 filelist = ["1-2000_years.txt", "2001-4000_years.txt", "4001-6000_years.txt", 
             "6001-8000_years.txt", "8001-10000_years.txt"]
 
-# TODO: Rename sample_dict to something more descriptive
-sample_dict = {}
-# make a set of countries
-countries = set()
+# Make a dictionary of sample IDs and the file they are in
+sample_to_file_dict = {}
 for file in filelist:
     with open(f"Data/{file}", "r") as sample:
         # skip the first line
@@ -22,18 +20,16 @@ for file in filelist:
         for line in sample:
             a = line.strip()
             b = a.split("\t")
-            countries.add(b[0])
             key = tuple(b)
-            sample_dict[key] = file
+            sample_to_file_dict[key] = file
 
-# convert set to a list
-countries = list(countries)
 # Get the fifth column of the bim file
-# TODO: what do they mean?
+# bim_col5 contains the values of the MAF and bim_col2 contains the SNP IDs
 bim_col5 = bim.iloc[:, 4]
 bim_col2 = bim.iloc[:, 1]
 
-# TODO: What does this do?
+
+#Make a list of dictionaries, one for each file
 year_country_dicts = [{} for _ in range(len(filelist))]
 #Make a set of all the SNPs that exist in my json files
 available_snps = set()
@@ -41,32 +37,33 @@ available_snps = set()
 with open('DataS1.ped', newline='') as csvfile:
     # Create a CSV reader object, delimited by spaces
     reader = csv.reader(csvfile, delimiter=' ')
-    # TODO: i is unnecessary
-    i = 0
     for row in reader:
-        # TODO: What is this key?
+        # key is a tuple of the first two columns of the ped file
         key = tuple(row[0:2])
-        if key in sample_dict:
-            # If the key is found, increment the frequency counter for each occurrence of the value
-            # TODO: update this comment
-            file = sample_dict[key]
+        if key in sample_to_file_dict:
+            # if the key is in the dictionary, get the file name    
+            file = sample_to_file_dict[key]
             file_index = filelist.index(file)
             country = row[0]
-            # TODO: country_index and countries is not needed
-            country_index = countries.index(country)
-            # TODO: Explain what this does
+            #if the country is not in the dictionary, add it
+            #if the country is in the dictionary, increment the value
+            #else add the country to the dictionary and set the value to 1
             if 'individual_num' not in year_country_dicts[file_index]:
                 year_country_dicts[file_index]['individual_num'] = {}
             if country in year_country_dicts[file_index]['individual_num']:
                 year_country_dicts[file_index]['individual_num'][country] += 1
             else:
                 year_country_dicts[file_index]['individual_num'][country] = 1
-            # TODO: Explain this
+            #for each pair of columns, check if the value is the same as the value in the bim file
             for start_col, end_col in zip(range(6, len(row), 2), range(7, len(row), 2)):
                 snp_index = (start_col-6)//2
                 snp = bim_col2.iloc[snp_index]
                 value = str(bim_col5.iloc[snp_index])
-
+                #if the value is the same as the value in the bim file, add the snp to the set of available snps
+                #if the snp is not in the dictionary, add it
+                #if the country is not in the dictionary, add it
+                #if the country is in the dictionary, increment the value   
+                #else add the country to the dictionary and set the value to 1
                 if row[start_col] == value:
                     available_snps.add(snp)
                     if snp not in year_country_dicts[file_index]:
@@ -83,8 +80,7 @@ with open('DataS1.ped', newline='') as csvfile:
                         year_country_dicts[file_index][snp][country] += 1
                     else:
                         year_country_dicts[file_index][snp][country] = 1
-            # TODO: Remove
-            i += 1
+
 for year, file in zip(year_country_dicts, filelist):
     with open("Data/{}.json".format(file), "w") as year_file:
         json.dump(year, year_file)
